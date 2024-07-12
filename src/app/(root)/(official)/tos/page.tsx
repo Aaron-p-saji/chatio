ae38012638609190b434bb92a9dd89588f4ef06e
Aaron-p-saji <aaronp.saji1233@gmail.com>
2024-07-12 18:54:36 +0530

    Added TOS

---
+++ src/app/(root)/(official)/tos/page.tsx
@@ -0,0 +1,249 @@
+import React from "react";
+
+type Props = {};
+
+const TermsOfService = (props: Props) => {
+  return (
+    <div className="h-screen py-8 px-4 overflow-y-auto scrollbar scrollbar-thumb-[#555] scrollbar-thumb-rounded-full scrollbar-w-2">
+      <div className="max-w-3xl mx-auto bg-white p-8 rounded-lg shadow-lg text-black">
+        <h1>Terms of Service</h1>
+        <p>Last updated: July 12, 2024</p>
+        <p>
+          Please read these terms and conditions carefully before using Our
+          Service.
+        </p>
+        <h2>Interpretation and Definitions</h2>
+        <h3>Interpretation</h3>
+        <p>
+          The words of which the initial letter is capitalized have meanings
+          defined under the following conditions. The following definitions
+          shall have the same meaning regardless of whether they appear in
+          singular or in plural.
+        </p>
+        <h3>Definitions</h3>
+        <p>For the purposes of these Terms and Conditions:</p>
+        <ul>
+          <li>
+            <p>
+              <strong>Affiliate</strong>{" "}
+              {`means an entity that controls, is
+              controlled by or is under common control with a party, where
+              "control" means ownership of 50% or more of the shares, equity
+              interest or other securities entitled to vote for election of
+              directors or other managing authority.`}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Company</strong>
+              {` (referred to as either "the Company",
+              "We", "Us" or "Our" in this Agreement) refers to Chatio.`}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Content</strong> refers to content such as text, images,
+              or other information that can be posted, uploaded, linked to or
+              otherwise made available by You, regardless of the form of that
+              content.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Service</strong> refers to the Application or the Website
+              or both.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Terms and Conditions</strong>{" "}
+              {`(also referred as "Terms")
+              mean these Terms and Conditions that form the entire agreement
+              between You and the Company regarding the use of the Service.
+            `}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Website</strong> refers to Chatio, accessible from{" "}
+              <a
+                href="https://chatio-sage.vercel.app/"
+                rel="external nofollow noopener"
+                target="_blank"
+              >
+                https://chatio-sage.vercel.app/
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>You</strong> means the individual accessing or using the
+              Service, or the company, or other legal entity on behalf of which
+              such individual is accessing or using the Service, as applicable.
+            </p>
+          </li>
+        </ul>
+        <h2>Acknowledgment</h2>
+        <p>
+          These are the Terms and Conditions governing the use of this Service
+          and the agreement that operates between You and the Company. These
+          Terms and Conditions set out the rights and obligations of all users
+          regarding the use of the Service.
+        </p>
+        <p>
+          Your access to and use of the Service is conditioned on Your
+          acceptance of and compliance with these Terms and Conditions. These
+          Terms and Conditions apply to all visitors, users and others who
+          access or use the Service.
+        </p>
+        <p>
+          By accessing or using the Service You agree to be bound by these Terms
+          and Conditions. If You disagree with any part of these Terms and
+          Conditions then You may not access the Service.
+        </p>
+        <h2>Accounts</h2>
+        <p>
+          When You create an account with Us, You must provide Us information
+          that is accurate, complete, and current at all times. Failure to do so
+          constitutes a breach of the Terms, which may result in immediate
+          termination of Your account on Our Service.
+        </p>
+        <p>
+          You are responsible for safeguarding the password that You use to
+          access the Service and for any activities or actions under Your
+          password, whether Your password is with Our Service or a Third-Party
+          Social Media Service.
+        </p>
+        <p>
+          You agree not to disclose Your password to any third party. You must
+          notify Us immediately upon becoming aware of any breach of security or
+          unauthorized use of Your account.
+        </p>
+        <h2>Intellectual Property</h2>
+        <p>
+          The Service and its original content (excluding Content provided by
+          You or other users), features and functionality are and will remain
+          the exclusive property of the Company and its licensors.
+        </p>
+        <p>
+          The Service is protected by copyright, trademark, and other laws of
+          both the Country and foreign countries. Our trademarks and trade dress
+          may not be used in connection with any product or service without the
+          prior written consent of the Company.
+        </p>
+        <h2>Links to Other Websites</h2>
+        <p>
+          Our Service may contain links to third-party web sites or services
+          that are not owned or controlled by the Company.
+        </p>
+        <p>
+          The Company has no control over, and assumes no responsibility for,
+          the content, privacy policies, or practices of any third party web
+          sites or services. You further acknowledge and agree that the Company
+          shall not be responsible or liable, directly or indirectly, for any
+          damage or loss caused or alleged to be caused by or in connection with
+          the use of or reliance on any such content, goods or services
+          available on or through any such web sites or services.
+        </p>
+        <p>
+          We strongly advise You to read the terms and conditions and privacy
+          policies of any third-party web sites or services that You visit.
+        </p>
+        <h2>Termination</h2>
+        <p>
+          We may terminate or suspend Your account immediately, without prior
+          notice or liability, for any reason whatsoever, including without
+          limitation if You breach these Terms and Conditions.
+        </p>
+        <p>
+          Upon termination, Your right to use the Service will cease
+          immediately. If You wish to terminate Your account, You may simply
+          discontinue using the Service.
+        </p>
+        <h2>Limitation of Liability</h2>
+        <p>
+          Notwithstanding any damages that You might incur, the entire liability
+          of the Company and any of its suppliers under any provision of this
+          Terms and Your exclusive remedy for all of the foregoing shall be
+          limited to the amount actually paid by You through the Service or 100
+          USD if You {"haven't"} purchased anything through the Service.
+        </p>
+        <p>
+          To the maximum extent permitted by applicable law, in no event shall
+          the Company or its suppliers be liable for any special, incidental,
+          indirect, or consequential damages whatsoever (including, but not
+          limited to, damages for loss of profits, loss of data or other
+          information, for business interruption, for personal injury, loss of
+          privacy arising out of or in any way related to the use of or
+          inability to use the Service, third-party software and/or third-party
+          hardware used with the Service, or otherwise in connection with any
+          provision of this Terms), even if the Company or any supplier has been
+          advised of the possibility of such damages and even if the remedy
+          fails of its essential purpose.
+        </p>
+        <p>
+          Some states do not allow the exclusion of implied warranties or
+          limitation of liability for incidental or consequential damages, which
+          means that some of the above limitations may not apply. In these
+          states, each {"party's"} liability will be limited to the greatest
+          extent permitted by law.
+        </p>
+        <h2>Disclaimer</h2>
+        <p>
+          Your use of the Service is at Your sole risk. The Service is provided
+          on an AS IS and AS AVAILABLE basis. The Service is provided without
+          warranties of any kind, whether express or implied, including, but not
+          limited to, implied warranties of merchantability, fitness for a
+          particular purpose, non-infringement, or course of performance.
+        </p>
+        <p>
+          The Company its subsidiaries, affiliates, and its licensors do not
+          warrant that a) the Service will function uninterrupted, secure or
+          available at any particular time or location; b) any errors or defects
+          will be corrected; c) the Service is free of viruses or other harmful
+          components; or d) the results of using the Service will meet Your
+          requirements.
+        </p>
+        <h2>Governing Law</h2>
+        <p>
+          These Terms shall be governed and construed in accordance with the
+          laws of Kerala, India, without regard to its conflict of law
+          provisions.
+        </p>
+        <p>
+          Our failure to enforce any right or provision of these Terms will not
+          be considered a waiver of those rights. If any provision of these
+          Terms is held to be invalid or unenforceable by a court, the remaining
+          provisions of these Terms will remain in effect. These Terms
+          constitute the entire agreement between us regarding our Service, and
+          supersede and replace any prior agreements we might have between us
+          regarding the Service.
+        </p>
+        <h2>Changes to These Terms and Conditions</h2>
+        <p>
+          We reserve the right, at Our sole discretion, to modify or replace
+          these Terms at any time. If a revision is material We will make
+          reasonable efforts to provide at least 30 days notice prior to any new
+          terms taking effect. What constitutes a material change will be
+          determined at Our sole discretion.
+        </p>
+        <p>
+          By continuing to access or use Our Service after those revisions
+          become effective, You agree to be bound by the revised terms. If You
+          do not agree to the new terms, in whole or in part, please stop using
+          the website and the Service.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions about these Terms and Conditions, You can
+          contact Us:
+        </p>
+        <ul>
+          <li>By email: aaronp.saji1233@gmail.com</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TermsOfService;
